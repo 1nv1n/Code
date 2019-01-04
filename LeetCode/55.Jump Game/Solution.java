6072ea51cc8add455abacf9bcb1f29b4d92829db
Neil Pathare <neil.pathare@outlook.com>
2019-01-04 17:22:37 -0600

    Updating Description & Adding Solution

--- LeetCode/55.Jump Game/Solution.java
+++ LeetCode/55.Jump Game/Solution.java
@@ -1,12 +1,61 @@
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 /**
  * Contains the logic that solves the provided problem.
  */
 class Solution {
+	// List containing indexes of possible jumps, in reverse order
+	private List<Integer> jumpList = null;
+	
 	public boolean canJump(int[] input) {
-		return true;
+		// Base case (empty array)
+		if(input.length <= 0) {
+			return false;
+		}
+
+		// Base case (single element array)
+		if(input.length == 1) {
+			return true;
+		}
+
+		// Set the initial current & target indexes.
+		int currIdx = input.length-2;
+		int targetIdx = input[input.length-1];
+
+		// Initialize & set the jumpList's last jump.
+		jumpList = new ArrayList<Integer>(input.length);
+		jumpList.add(input[input.length-1]);
+		
+		// Loop from the second-to-the-last array index
+		// decerementing till we're down to the 0th array index.
+		while(currIdx >= 0) {
+			// Check whether we can reach the target index from the current index.
+			if(currIdx + input[currIdx] >= targetIdx) {
+				// If we can sucessfully jump from the current index to the target index,
+				// Add the current index to the jumpList (so we know that there is a path from this index to the target index)
+				// Set the current index to be the target index for the next run.
+				jumpList.add(currIdx);
+				targetIdx = currIdx;
+			}
+
+			// Next loop for the index before the current index.
+			currIdx--;
+		}
+
+		// Check the status of our jumpList
+		System.out.println("Jump Indexes in Reverse:"+jumpList);
+
+		// If we have the start index in the jumpList
+		//  it means that we can sucessfully go from the start index
+		//  to the end index.
+		if(jumpList.get(jumpList.size()-1) == 0) {
+			return true;
+		}
+
+		return false;
 	}
 }
 
