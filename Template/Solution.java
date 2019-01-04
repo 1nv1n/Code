3227186e814f7e864b4a4bcd764b67d48d44cf74
Neil Pathare <neil.pathare@outlook.com>
2019-01-04 16:21:20 -0600

    Updating Template

--- Template/Solution.java
+++ Template/Solution.java
@@ -1,16 +1,20 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 /**
- * Contains the entry point of the solution.
+ * Contains the loigc that solves the provided problem.
  */
 class Solution {
+
 }
 
 /**
- * Driver class for the problem's solution.
+ * Driver class for the problem's solution (JDK8+).
  */
 public class MainClass {
-  private static DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+	private static String currTimeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(Calendar.getInstance().getTime());
 
-  public static void main(String[] args) throws IOException {
-    System.out.println(dateTimeFormatter.format(LocalDateTime.now()));
-  }
+	public static void main(String[] args) {
+		System.out.println(currTimeStamp);
+	}
 }
