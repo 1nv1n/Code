3f9e117367ef178fc25c48fdd4a2391bdf3b71b6
Neil Pathare <neil.pathare@outlook.com>
2019-03-04 15:33:07 -0600

    Updated Solution & Added Analysis

--- Practice/LeetCode/AddDigits/Solution.java
+++ Practice/LeetCode/AddDigits/Solution.java
@@ -1,17 +1,62 @@
 /**
- * Class that contains the logic & implementation for the provided problem's solution.
+ * Solution Implementor.
  */
 class CodeGoesHere {
+  /**
+  * Returns the sum of the digits in the given number in constant time.
+  */
+  public int addDigitsConstantTime(int num) {
+    // Base case, if the number is lesser than 10 or invalid (negative); just return it.
+    if(num < 10) {
+      return num;
+    }
+
+    // Math magic for constant time digit-add
+    return num % 9 == 0 ? 9 : num % 9;
+  }
+
+  /**
+  * Returns the sum of the digits in the given number.
+  */
+  public int addDigits(int num) {
+    // Assume a minimal sum
+    int sum = 0;
+
+    // String placeholder
+    String sumStr = "";
+
+    // Base case, if the number is lesser than 10 or invalid (negative); return 0.
+    if(num < 10) {
+      return sum;
+    } else {
+      sum = num;
+    }
+
+    // Keep calculating until the final sum is less than 2 digits
+    while(sum > 10) {
+      sumStr = Integer.toString(sum);
+      sum = 0;
+
+      // Add up every element in the array to re-calculate the sum.
+      for(char c : sumStr.toCharArray()) {
+        sum += Character.getNumericValue(c);
+      }
+    }
+
+    return sum;
+  }
 }
 
 /**
- * This is the driver of the solution.
- * This class is not expected to hold any logic,
- *  but rather to call into the method(s) from {@link CodeGoesHere} to perform the expected actions.
+ * Driver.
  */
-public class SolutionDriver {
-	public static void main(String[] args) {
-		CodeGoesHere cGH = new CodeGoesHere();
-		System.out.println("");
-	}
+public class Solution {
+  /**
+   * Entry point.
+   * @param args {@link String} argument array from the standard input.
+   */
+  public static void main(String[] args) {
+    CodeGoesHere cGH = new CodeGoesHere();
+    System.out.println(cGH.addDigits(38));
+  }
 }
