f58c16f3484f70f5f9847e90733e2798ad1411ad
Neil Pathare <neil.pathare@outlook.com>
2019-07-16 23:07:05 -0500

    CGH - Refactor Update

--- Practice/LeetCode/TwoSum/Solution.java
+++ Practice/LeetCode/TwoSum/Solution.java
@@ -1,17 +1,47 @@
+import java.util.Arrays;
+import java.util.Map;
+import java.util.HashMap;
+
 /**
- * Class that contains the logic & implementation for the provided problem's solution.
+ * Solution Implementor.
  */
 class CodeGoesHere {
+  public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> complementMap;
+
+    // Input validation
+    if(nums == null || nums.length <= 0) {
+      throw new IllegalArgumentException("Invalid input!");
+    } else {
+      complementMap = new HashMap<>(nums.length);
+    }
+
+    // Visit every element
+    for(int nIdx = 0; nIdx < nums.length; nIdx++) {
+      int complement = target - nums[nIdx];
+
+      // Check if the number's complement exists
+      if(complementMap.containsKey(complement) && complementMap.get(complement) != nIdx) {
+        return new int[]{complementMap.get(complement), nIdx};
+      }
+
+      complementMap.put(nums[nIdx], nIdx);
+    }
+
+    throw new IllegalStateException("No Two Sum Solution!");
+  }
 }
 
 /**
- * This is the driver of the solution.
- * This class is not expected to hold any logic,
- *  but rather to call into the method(s) from {@link CodeGoesHere} to perform the expected actions.
+ * Driver.
  */
-public class SolutionDriver {
-	public static void main(String[] args) {
-		CodeGoesHere cGH = new CodeGoesHere();
-		System.out.println("");
-	}
+public class Solution {
+  /**
+   * Entry point.
+   * @param args {@link String} argument array from the standard input.
+   */
+  public static void main(String[] args) {
+    CodeGoesHere cGH = new CodeGoesHere();
+    System.out.println(Arrays.toString(cGH.twoSum(new int[]{2, 7, 11, 15}, 9)));
+  }
 }
